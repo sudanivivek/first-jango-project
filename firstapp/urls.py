83784b75959141cfb7a6812b6118ad64c0a33ee8
Vivek <laljibhai108@gmail.com>
2024-09-26 16:12:12 +0530

    remove code

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from .views import edit_profile/
 from .views import register, login,dashboard
 from .views import profile,logout,update
 from django.urls import path
@@ -12,13 +11,4 @@ urlpatterns = [
     path('profile',profile,name='profile'),
     path('logout/', logout, name='logout'),
     path('update/',update,name='update'),
-    # path('edit-profile/', edit_profile, name='edit_profile'),   
-    
 ]
-    # path('', views.register, name='register'),
-    # path('login/', views.login_view, name='login'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
-    # path('profile/', views.profile, name='profile'),
-    # path('logout/', views.logout_view, name='logout'),
-    # path('password-reset/', views.password_reset_request, name='password_reset_request'),
-    # path('password-reset/confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
